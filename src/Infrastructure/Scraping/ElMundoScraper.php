a3fce82a83abae530f3f2f88259369c4bc599f53
Jose <hello@innela.com>
2026-01-08 21:25:56 +0100

    feat(scraping): add El Mundo scraper implementation

---
+++ src/Infrastructure/Scraping/ElMundoScraper.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Infrastructure\Scraping;
+
+use App\Domain\Enum\SourceEnum;
+
+class ElMundoScraper extends AbstractHtmlScraper
+{
+    public function getSource(): SourceEnum
+    {
+        return SourceEnum::EL_MUNDO;
+    }
+
+    protected function getUrl(): string
+    {
+        return 'https://www.elmundo.es';
+    }
+
+    protected function getSelectors(): array
+    {
+        return [
+            'article_link' => 'a.ue-c-cover-content__link, article header a, article h2 a',
+
+            'title' => [
+                'h1.ue-c-article__headline',
+                'h1.ue-c-cover-content__headline',
+                'h1'
+            ],
+
+            'body' => [
+                '.ue-c-article__body p',
+                'div[data-section="articleBody"] p',
+                'main article p'
+            ],
+            'image' => [
+                '.ue-c-article__media img',
+                'article figure img',
+                '.ue-c-article__image'
+            ],
+
+            'date' => [
+                'time',
+                '.ue-c-article__publishdate time',
+                '.ue-c-article__publishdate'
+            ]
+        ];
+    }
+}
